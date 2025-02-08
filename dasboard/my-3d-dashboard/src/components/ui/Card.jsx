db36b615957ca4f956037f49f2642ee151a7ffe4
aditya paswan <cs.aditya58@gmail.com>
2025-02-08 10:37:54 +0530

    submission

--- dasboard/my-3d-dashboard/src/components/ui/Card.jsx
+++
@@ -1,9 +0,0 @@
-const Card = ({ children, className = '' }) => {
-    return (
-      <div className={`bg-white rounded-lg shadow-lg p-4 ${className}`}>
-        {children}
-      </div>
-    );
-  };
-  
-  export default Card;
